0de3753450969c4c204112ec0186a76490ab88ca
Jihong Park <ggikko@hotmail.com>
2016-04-21 13:11:30 +0900

    before having a lunch, commit :)

--- ExpandableRecyclerviewGrade/app/src/main/java/ggikko/me/expandablerecyclerviewgrade/MainActivity.java
+++ ExpandableRecyclerviewGrade/app/src/main/java/ggikko/me/expandablerecyclerviewgrade/MainActivity.java
@@ -2,12 +2,26 @@ package ggikko.me.expandablerecyclerviewgrade;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.recyclerview) RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        RecyclerviewAdapter adapter = new RecyclerviewAdapter(this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
     }
 }
---
+++ ExpandableRecyclerviewGrade/app/src/main/java/ggikko/me/expandablerecyclerviewgrade/RecyclerviewAdapter.java
@@ -0,0 +1,31 @@
+package ggikko.me.expandablerecyclerviewgrade;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by admin on 16. 4. 21..
+ */
+public class RecyclerviewAdapter extends RecyclerView.Adapter {
+
+
+    public RecyclerviewAdapter(Context context) {
+
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
